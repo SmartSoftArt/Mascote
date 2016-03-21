ce12e2c881ec2c68261359099766a91dddc73842
Tamapw <tigand@mail.ru>
2016-03-21 22:24:44 +0300

    Анимация тип

--- MascotAndrey/Mascot/MainWindow.xaml.cs
+++ MascotAndrey/Mascot/MainWindow.xaml.cs
@@ -33,15 +33,26 @@ namespace Mascot {
     }
 
     private void TimerTick(object sender, EventArgs e) {
+
       if (i) {
-        image.ImageSource = new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "img/shime1.png"));
+        image.ImageSource = new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "img/shime3.png"));
         //grid.Background = image;
         this.Background = image;
         i = false;
       } else {
-        image.ImageSource = new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "img/shime1b.png"));
+        image.ImageSource = new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "img/shime2.png"));
         //grid.Background = image;
         this.Background = image;
+        if (this.Left > 100) {
+          this.Left -= 10;
+        } else {
+          System.Windows.Threading.DispatcherTimer timer = (System.Windows.Threading.DispatcherTimer)sender;
+          timer.Stop();
+          image.ImageSource = new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "img/shime1.png"));
+          //grid.Background = image;
+          this.Background = image;
+        }
+
         i = true;
       }
     }
