fff8c68a6b6afff71514a55b60153efd2d968777
Иван Кривцов <vankrivcov9@gmail.com>
2016-03-22 21:20:21 +0600

    Контекстное меню трея (изменен только MainWindow.xaml.cs) Версия на 22.03 21:16
    Signed-off-by: Иван Кривцов <vankrivcov9@gmail.com>

---
+++ Mascote (v.22.03. 21.16)/Mascote-Tama/MascotAndrey/Mascot/Animation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Threading;
+
+
+namespace Mascot {
+  class AnimationMascote {
+    static public void Animation(Grid mascote, string WayMascote) {
+      mascote.Background = new ImageBrush(new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(mascote), WayMascote)));
+    }
+  }
+}
