977e45daa09ae3bd85a0a340b36f9b86d5e82860
Tamapw <tigand@mail.ru>
2016-03-23 16:25:54 +0300

    Уведомление с анимацией

--- MascotAndrey/Mascot/MainWindow.xaml.cs
+++ MascotAndrey/Mascot/MainWindow.xaml.cs
@@ -21,60 +21,62 @@ namespace Mascot {
   /// Логика взаимодействия для MainWindow.xaml
   /// </summary>
   /// 
-    public partial class MainWindow : Window {
+  public partial class MainWindow : Window {
 
-      private System.Windows.Forms.ContextMenu contextMenu1;
-      private System.Windows.Forms.MenuItem menuItem1;
+    private System.Windows.Forms.ContextMenu contextMenu1;
+    private System.Windows.Forms.MenuItem menuItem1;
+    System.Windows.Threading.DispatcherTimer timer;
 
-      Mascote mascote;
-        public MainWindow() 
-        {
-            InitializeComponent();
-            mascote = new Mascote(grid, MainWindow1);
-            //Таймер для анимации
-            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
-            timer.Tick += new EventHandler(TimerTick);
-            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
-            timer.Start();
-            //Описание трея
-            System.Windows.Forms.NotifyIcon tray = new System.Windows.Forms.NotifyIcon();
-            tray.Icon = new System.Drawing.Icon("icon.ico");
-            tray.Visible = true;
-            tray.MouseClick += tray_MouseClick;
-            //Создание объекта контекстного меню и его элемента(ов)
-            this.contextMenu1 = new System.Windows.Forms.ContextMenu();
-            this.menuItem1 = new System.Windows.Forms.MenuItem();
-            this.contextMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] { this.menuItem1 });
+    Mascote mascote;
+    public MainWindow() {
+      InitializeComponent();
+      mascote = new Mascote(grid, MainWindow1);
+      //Таймер для анимации
+      timer = new System.Windows.Threading.DispatcherTimer();
+      timer.Tick += new EventHandler(TimerTick);
+      timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+      timer.Start();
+      //Описание трея
+      System.Windows.Forms.NotifyIcon tray = new System.Windows.Forms.NotifyIcon();
+      tray.Icon = new System.Drawing.Icon("icon.ico");
+      tray.Visible = true;
+      tray.MouseClick += tray_MouseClick;
+      //Создание объекта контекстного меню и его элемента(ов)
+      this.contextMenu1 = new System.Windows.Forms.ContextMenu();
+      this.menuItem1 = new System.Windows.Forms.MenuItem();
+      this.contextMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] { this.menuItem1 });
 
-            // Инициализация элемента
-            this.menuItem1.Index = 0;
-            this.menuItem1.Text = "E&xit";
-            this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+      // Инициализация элемента
+      this.menuItem1.Index = 0;
+      this.menuItem1.Text = "Exit";
+      this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
 
-            //Инициализация контекстного меню
-            tray.ContextMenu = this.contextMenu1;
+      //Инициализация контекстного меню
+      tray.ContextMenu = this.contextMenu1;
 
-        }
-
-        void tray_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e) {
-            if (e.Button == System.Windows.Forms.MouseButtons.Left){
-              notification.IsOpen = true;
-            }
-        }
-        private void menuItem1_Click(object sender, EventArgs e)
-        {
-            {
-                this.Close();
-                Process.GetCurrentProcess().Kill();
-            }
-        }
-        private void TimerTick(object sender, EventArgs e) 
-        {
-          mascote.ActionMascote();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e) {
-          notification.IsOpen = false;
-        }
     }
+
+    void tray_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e) {
+      if (e.Button == System.Windows.Forms.MouseButtons.Left) {
+        notification.IsOpen = true;
+        mascote.threadMascot.Suspend();
+        mascote.NumAction = 2;
+
+      }
+    }
+    private void menuItem1_Click(object sender, EventArgs e) {
+      {
+        this.Close();
+        Process.GetCurrentProcess().Kill();
+      }
+    }
+    private void TimerTick(object sender, EventArgs e) {
+      mascote.ActionMascote();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e) {
+      mascote.threadMascot.Resume();
+      notification.IsOpen = false;
+    }
+  }
 }
--- MascotAndrey/Mascot/Mascot.cs
+++ MascotAndrey/Mascot/Mascot.cs
@@ -15,110 +15,131 @@ using System.Windows.Shapes;
 using System.Windows.Media.Animation;
 using System.Threading;
 
-namespace Mascot
-{
-    class Mascote
-    {
-        private volatile int numAction = 0;
-        private volatile bool step = true;
-        private volatile int xToMove;
-        private string wayShimeOne;
-        private string wayWhimeTwo;
-        private bool toRight;
-        private Thread threadMascot;
-        private Random random = new Random();
-        private MainWindow window;
-        private Grid grid;
-        private int ScreenHeight = (int)SystemParameters.PrimaryScreenHeight;
-        private int ScreenWidth = (int)SystemParameters.PrimaryScreenWidth;
-        private int gridWidth;
+namespace Mascot {
+  class Mascote {
+    private volatile int numAction = 0;
+    private volatile int step = 1;
+    private volatile int xToMove;
+    private string wayShimeOne;
+    private string wayWhimeTwo;
+    private List<string> wayShime;
+    private bool toRight;
+    public  Thread threadMascot;
+    private Random random = new Random();
+    private MainWindow window;
+    private Grid grid;
+    private int ScreenHeight = (int)SystemParameters.PrimaryScreenHeight;
+    private int ScreenWidth = (int)SystemParameters.PrimaryScreenWidth;
+    private int gridWidth;
+    public int NumAction { set { numAction = value; } }
 
 
-        public Mascote(Grid mascote, MainWindow mainwindow)
-        {
-            threadMascot = new Thread(MascotAI);
-            threadMascot.Start();
-            grid = mascote;
-            gridWidth = (int)grid.Width;
-            window = mainwindow;
-        }
-
-        private void MascotAI()
-        {
-            while (true)
-            {
-                Thread.Sleep(random.Next(5000));
-                int buf = random.Next(2);
-                if (buf == 1)
-                {
-                    xToMove = xToMove = ScreenWidth - random.Next(341) - gridWidth;
-                }
-                numAction = buf;
-            }
-        }
-        public void ActionMascote()
-        {
-            switch (numAction)
-            {
-                case 0:
-                    {
-                        this.doStand();
-                        break;
-                    }
-                case 1:
-                    {
-                        if (window.Left > xToMove)
-                        {
-                            wayShimeOne = "img/mov/shime2.png";
-                            wayWhimeTwo = "img/mov/shime3.png";
-                            toRight = false;
-                        }
-                        else
-                        {
-                            wayShimeOne = "img/mov/shime2R.png";// shime 2
-                            wayWhimeTwo = "img/mov/shime3R.png";// shime 3
-                            toRight = true;
-                        }
-
-                        if (step)
-                        {
-                            AnimationMascote.Animation(grid, wayWhimeTwo);
-                            step = false;
-                        }
-                        else
-                        {
-                            AnimationMascote.Animation(grid, wayShimeOne);
-                            if (toRight == true)
-                            {
-                                if (window.Left < xToMove)
-                                {
-                                    window.Left += 10;
-                                }
-                                else
-                                {
-                                    numAction = 0;
-                                }
-                            }
-                            else
-                            {
-                                if (window.Left > xToMove)
-                                {
-                                    window.Left -= 10;
-                                }
-                                else
-                                {
-                                    numAction = 0;
-                                }
-                            }
-                            step = true;
-                        }
-                        break;
-                    }
-            }
-        }
-        private void doStand()
-        {
-            AnimationMascote.Animation(grid, "img/shime1.png");
-        }
+    public Mascote(Grid mascote, MainWindow mainwindow) {
+      wayShime = new List<string>();
+      for (int i = 0; i < 3; i++) {
+        wayShime.Add("");
+      }
+      threadMascot = new Thread(MascotAI);
+      threadMascot.Start();
+      grid = mascote;
+      gridWidth = (int)grid.Width;
+      window = mainwindow;
     }
+
+    private void MascotAI() {
+      while (true) {
+        Thread.Sleep(random.Next(5000));
+        int buf = random.Next(2);
+        if (buf == 1) {
+          xToMove = xToMove = ScreenWidth - random.Next(341) - gridWidth;
+        }
+        numAction = buf;
+      }
+    }
+
+
+    public void ActionMascote() {
+      switch (numAction) {
+        // 0 - стоять на месте
+        // 1 - ходить в какую-либо сторону
+        // 2 - анимация во время оповещения
+        //
+        case 0: {
+            this.doStand();
+            break;
+          }
+        case 1: {
+            if (window.Left > xToMove) {
+              wayShime[0] = "img/mov/shime2.png";
+              wayShime[1] = "img/mov/shime3.png";
+              toRight = false;
+            } else {
+              wayShime[0] = "img/mov/shime2R.png";// shime 2
+              wayShime[1] = "img/mov/shime3R.png";// shime 3
+              toRight = true;
+            }
+            switch (step) {
+              case 0: {
+                  AnimationMascote.Animation(grid, wayShime[step]);
+                  if (toRight == true) {
+                    if (window.Left < xToMove) {
+                      window.Left += 10;
+                    } else {
+                      numAction = 0;
+                    }
+                  } else {
+                    if (window.Left > xToMove) {
+                      window.Left -= 10;
+                    } else {
+                      numAction = 0;
+                    }
+                  }
+                  step = 1;
+                  break;
+                }
+              case 1: {
+                  AnimationMascote.Animation(grid, wayShime[step]);
+                  step = 0;
+                  break;
+                }
+              default: {
+                step = 0;
+                break;
+                }
+            }
+          break;
+          }
+        case 2: {
+          wayShime[0] = ("img/shime31.png");
+          wayShime[1] = ("img/shime32.png");
+          wayShime[2] = ("img/shime33.png");
+          AnimationMascote.Animation(grid, wayShime[step]);
+          switch (step) {
+            case 0: {
+              step = 1;
+              break;
+            }
+
+            case 1: {
+                step = 2;
+                break;
+            }
+
+            case 2: {
+                step = 0;
+                break;
+            }
+          }
+
+          break;
+          }
+      }
+    }
+    private void doStand() {
+      AnimationMascote.Animation(grid, "img/shime1.png");
+    }
+    public void ThreadAIStop () {
+      threadMascot.Suspend();
+    }
+  }
 }
